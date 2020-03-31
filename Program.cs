a89942fbed83dc1865c69cd48a69e1dc6565c4fc
gregorix <gregorixpd0809@gmail.com>
2020-03-31 15:37:59 -0400

    Singleton

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Singleton.Instance.mensaje);
+
+            Console.ReadKey();
+
+            Singleton.Instance.mensaje = " *Actualizado*, Hola Benjamin";
+
+            Console.WriteLine(Singleton.Instance.mensaje);
+
+            Console.ReadKey();
+
+        }
+    }
+}
